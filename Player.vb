95d33917fc65c58eceb31fc724cb9996f2b8a68e
jstudz <jstudzinski1@my.wctc.edu>
2017-08-10 09:19:07 -0500

    Initial Commit
    Add player

---
+++ Player.vb
@@ -0,0 +1,19 @@
+﻿'Holds the value for each hand (computer and user)
+Public Class Player
+    Dim hand_Total As Integer
+    'Sets the hand total to 0 
+    Public Sub New()
+        hand_Total = 0
+    End Sub
+    'Encapsulates to a read only file so it can be read by the program but not edited
+    Public Property handTotal As Integer
+        Get
+            Return hand_Total
+        End Get
+        'Grabs the value from the cards and sets it to the hand total
+        Set(ByVal value As Integer)
+            hand_Total = value
+        End Set
+    End Property
+
+End Class
